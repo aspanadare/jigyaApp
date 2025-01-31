54e72369eac0d3b18c6e6856cd93acc6d063cce0
aspanadare <aniruddhapanadare@gmail.com>
2025-01-31 19:34:38 +0530

    working code with UI

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -35,11 +35,11 @@ const FeedScreen = () => {
 export default FeedScreen
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
      
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-});
+    },
+  });
