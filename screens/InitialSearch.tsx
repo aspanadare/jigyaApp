f6cf5b654cb31275da577676cb12be3c2db05510
Abhay Mittal <abhay.mittal@jigserv.com>
2025-02-10 11:24:15 +0530

    feat: add search result page

--- screens/InitialSearch.tsx
+++ screens/InitialSearch.tsx
@@ -7,6 +7,7 @@ import {
   TextInput,
   TouchableOpacity,
   ScrollView,
+  Keyboard,
 } from "react-native";
 import React, { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -24,14 +25,22 @@ const InitialSearch = () => {
     "Sports",
   ];
   const [selected, setSelected] = useState("Technology");
-  const [isSearchFocused, setIsSearchFocused] = useState(false); 
+  const [isSearchFocused, setIsSearchFocused] = useState(false);
+  const [isSearchPerformed, setIsSearchPerformed] = useState(false);
   const handleBlur = () => {
     setIsSearchFocused(false);
   };
   const handleFocus = () => {
     setIsSearchFocused(true);
   };
-  const shouldShowContent = !isSearchFocused && searchText === "";
+  const handleSearch = () => {
+    if (searchText.trim() !== "") {
+      setIsSearchPerformed(true);
+      Keyboard.dismiss(); // Dismiss the keyboard
+    }
+  };
+  const shouldShowContent =
+    !isSearchFocused && searchText === "" && !isSearchPerformed;
 
   return (
     <SafeAreaView className="bg-white">
@@ -47,177 +56,185 @@ const InitialSearch = () => {
           placeholder={"Search Insight/Jobs/Courses"}
           value={searchText}
           onChangeText={setSearchText}
-          onFocus={handleFocus} 
+          onFocus={handleFocus}
+          onBlur={handleBlur}
           placeholderTextColor="#CFCFCF"
+          onSubmitEditing={handleSearch}
         />
       </View>
       {shouldShowContent && (
-      <ScrollView contentContainerStyle={{ flexGrow: 1, minHeight: 750 }}>
-        <View className="flex flex-col pl-3 pr-4">
-          <ScrollView
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            className="flex-row"   
-          >
-            {categories.map((category, index) => (
-              <Pressable
-                key={index}
-                onPress={() => setSelected(category)}
-                className={`px-4 py-2 mr-4 rounded-md border ${
-                  selected === category
-                    ? "bg-[#2DA5CB] border-0"
-                    : "bg-white border-[#707070]"
-                }`}
-              >
-                <Text
-                  className={`text-[14px] font-normal ${
-                    selected === category ? "text-white" : "text-[#707070]"
+        <ScrollView contentContainerStyle={{ flexGrow: 1, minHeight: 750 }}>
+          <View className="flex flex-col pl-3 pr-4">
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              className="flex-row"
+            >
+              {categories.map((category, index) => (
+                <Pressable
+                  key={index}
+                  onPress={() => setSelected(category)}
+                  className={`px-4 py-2 mr-4 rounded-md border ${
+                    selected === category
+                      ? "bg-[#2DA5CB] border-0"
+                      : "bg-white border-[#707070]"
                   }`}
                 >
-                  {category}
-                </Text>
-              </Pressable>
-            ))}
-          </ScrollView>
-          <View className="flex flex-col gap-4">
-            <View>
-              <Text className="text-[16px] font-normal">Insights</Text>{" "}
-              <View className="w-[70px] h-1 bg-[#2DA5CB] mt-[10px] rounded-sm"></View>
-            </View>
-
+                  <Text
+                    className={`text-[14px] font-normal ${
+                      selected === category ? "text-white" : "text-[#707070]"
+                    }`}
+                  >
+                    {category}
+                  </Text>
+                </Pressable>
+              ))}
+            </ScrollView>
             <View className="flex flex-col gap-4">
-              <View className="w-full h-[54px] flex flex-row gap-4 items-center">
-                <View className="flex">
-                  <Image
-                    source={insightPic}
-                    alt="Profile"
-                    className="w-[54px] h-[54px] rounded-lg flex"
-                  />
-                </View>
-                <View className="flex flex-1">
-                  <Text className="text-[14px] font-normal">
-                    what makes an effective leader? important leadership
-                    Competencies
-                  </Text>
-                </View>
+              <View>
+                <Text className="text-[16px] font-normal">Insights</Text>{" "}
+                <View className="w-[70px] h-1 bg-[#2DA5CB] mt-[10px] rounded-sm"></View>
               </View>
-              <View className="w-full h-[54px] flex flex-row gap-4 items-center">
-                <View className="flex">
-                  <Image
-                    source={insightPic}
-                    alt="Profile"
-                    className="w-[54px] h-[54px] rounded-lg flex"
-                  />
-                </View>
-                <View className="flex flex-1">
-                  <Text className="text-[14px] font-normal">
-                    what makes an effective leader? important leadership
-                    Competencies
-                  </Text>
-                </View>
-              </View>
-            </View>
-            <TouchableOpacity className="w-full items-center">
-              {" "}
-              <Text className="text-[#2DA5CB] font-[14px]"> VIEW MORE</Text>
-            </TouchableOpacity>
-          </View>
-          <View className="flex flex-col gap-4">
-            <View>
-              <Text className="text-[16px] font-normal">Jobs</Text>{" "}
-              <View className="w-[40px] h-1 bg-[#2DA5CB] mt-[10px] rounded-sm"></View>
-            </View>
 
+              <View className="flex flex-col gap-4">
+                <View className="w-full h-[54px] flex flex-row gap-4 items-center">
+                  <View className="flex">
+                    <Image
+                      source={insightPic}
+                      alt="Profile"
+                      className="w-[54px] h-[54px] rounded-lg flex"
+                    />
+                  </View>
+                  <View className="flex flex-1">
+                    <Text className="text-[14px] font-normal">
+                      what makes an effective leader? important leadership
+                      Competencies
+                    </Text>
+                  </View>
+                </View>
+                <View className="w-full h-[54px] flex flex-row gap-4 items-center">
+                  <View className="flex">
+                    <Image
+                      source={insightPic}
+                      alt="Profile"
+                      className="w-[54px] h-[54px] rounded-lg flex"
+                    />
+                  </View>
+                  <View className="flex flex-1">
+                    <Text className="text-[14px] font-normal">
+                      what makes an effective leader? important leadership
+                      Competencies
+                    </Text>
+                  </View>
+                </View>
+              </View>
+              <TouchableOpacity className="w-full items-center">
+                {" "}
+                <Text className="text-[#2DA5CB] font-[14px]"> VIEW MORE</Text>
+              </TouchableOpacity>
+            </View>
             <View className="flex flex-col gap-4">
-              <View className="w-full h-[54px] flex flex-row gap-4 items-center">
-                <View className="flex">
-                  <Image
-                    source={insightPic}
-                    alt="Profile"
-                    className="w-[103px] h-[54px] rounded-lg flex"
-                  />
-                </View>
-                <View className="flex flex-1">
-                  <Text className="text-[14px] font-normal">
-                    Lead Salesforce developer
-                  </Text>
-                  <Text className="text-[12px] font-light italic">
-                    Induslnd Bank
-                  </Text>
-                </View>
+              <View>
+                <Text className="text-[16px] font-normal">Jobs</Text>{" "}
+                <View className="w-[40px] h-1 bg-[#2DA5CB] mt-[10px] rounded-sm"></View>
               </View>
-              <View className="w-full h-[54px] flex flex-row gap-4 items-center">
-                <View className="flex">
-                  <Image
-                    source={insightPic}
-                    alt="Profile"
-                    className="w-[103px] h-[54px] rounded-lg flex"
-                  />
-                </View>
-                <View className="flex flex-1">
-                  <Text className="text-[14px] font-normal">
-                    Lead Salesforce developer
-                  </Text>
-                  <Text className="text-[12px] font-light italic">
-                    Induslnd Bank
-                  </Text>
-                </View>
-              </View>
-            </View>
-            <TouchableOpacity className="w-full items-center">
-              {" "}
-              <Text className="text-[#2DA5CB] font-[14px]"> VIEW MORE</Text>
-            </TouchableOpacity>
-          </View>
-          <View className="flex flex-col gap-4">
-            <View>
-              <Text className="text-[16px] font-normal">Jobs</Text>{" "}
-              <View className="w-[64px] h-1 bg-[#2DA5CB] mt-[10px] rounded-sm"></View>
-            </View>
 
-            <View className="flex flex-col gap-4">
-              <View className="w-full h-[54px] flex flex-row gap-4 items-center">
-                <View className="flex">
-                  <Image
-                    source={insightPic}
-                    alt="Profile"
-                    className="w-[103px] h-[54px] rounded-lg flex"
-                  />
+              <View className="flex flex-col gap-4">
+                <View className="w-full h-[54px] flex flex-row gap-4 items-center">
+                  <View className="flex">
+                    <Image
+                      source={insightPic}
+                      alt="Profile"
+                      className="w-[103px] h-[54px] rounded-lg flex"
+                    />
+                  </View>
+                  <View className="flex flex-1">
+                    <Text className="text-[14px] font-normal">
+                      Lead Salesforce developer
+                    </Text>
+                    <Text className="text-[12px] font-light italic">
+                      Induslnd Bank
+                    </Text>
+                  </View>
                 </View>
-                <View className="flex flex-1">
-                  <Text className="text-[14px] font-normal">
-                    Women Leadership Programme
-                  </Text>
-                  <Text className="text-[12px] font-light italic">
-                    https://exced.isb.edu/
-                  </Text>
+                <View className="w-full h-[54px] flex flex-row gap-4 items-center">
+                  <View className="flex">
+                    <Image
+                      source={insightPic}
+                      alt="Profile"
+                      className="w-[103px] h-[54px] rounded-lg flex"
+                    />
+                  </View>
+                  <View className="flex flex-1">
+                    <Text className="text-[14px] font-normal">
+                      Lead Salesforce developer
+                    </Text>
+                    <Text className="text-[12px] font-light italic">
+                      Induslnd Bank
+                    </Text>
+                  </View>
                 </View>
               </View>
-              <View className="w-full h-[54px] flex flex-row gap-4 items-center">
-                <View className="flex">
-                  <Image
-                    source={insightPic}
-                    alt="Profile"
-                    className="w-[103px] h-[54px] rounded-lg flex"
-                  />
-                </View>
-                <View className="flex flex-1">
-                  <Text className="text-[14px] font-normal">
-                    Women Leadership Programme
-                  </Text>
-                  <Text className="text-[12px] font-light italic">
-                    https://exced.isb.edu/
-                  </Text>
-                </View>
-              </View>
+              <TouchableOpacity className="w-full items-center">
+                {" "}
+                <Text className="text-[#2DA5CB] font-[14px]"> VIEW MORE</Text>
+              </TouchableOpacity>
             </View>
-            <TouchableOpacity className="w-full items-center">
-              {" "}
-              <Text className="text-[#2DA5CB] font-[14px]"> VIEW MORE</Text>
-            </TouchableOpacity>
+            <View className="flex flex-col gap-4">
+              <View>
+                <Text className="text-[16px] font-normal">Jobs</Text>{" "}
+                <View className="w-[64px] h-1 bg-[#2DA5CB] mt-[10px] rounded-sm"></View>
+              </View>
+
+              <View className="flex flex-col gap-4">
+                <View className="w-full h-[54px] flex flex-row gap-4 items-center">
+                  <View className="flex">
+                    <Image
+                      source={insightPic}
+                      alt="Profile"
+                      className="w-[103px] h-[54px] rounded-lg flex"
+                    />
+                  </View>
+                  <View className="flex flex-1">
+                    <Text className="text-[14px] font-normal">
+                      Women Leadership Programme
+                    </Text>
+                    <Text className="text-[12px] font-light italic">
+                      https://exced.isb.edu/
+                    </Text>
+                  </View>
+                </View>
+                <View className="w-full h-[54px] flex flex-row gap-4 items-center">
+                  <View className="flex">
+                    <Image
+                      source={insightPic}
+                      alt="Profile"
+                      className="w-[103px] h-[54px] rounded-lg flex"
+                    />
+                  </View>
+                  <View className="flex flex-1">
+                    <Text className="text-[14px] font-normal">
+                      Women Leadership Programme
+                    </Text>
+                    <Text className="text-[12px] font-light italic">
+                      https://exced.isb.edu/
+                    </Text>
+                  </View>
+                </View>
+              </View>
+              <TouchableOpacity className="w-full items-center">
+                {" "}
+                <Text className="text-[#2DA5CB] font-[14px]"> VIEW MORE</Text>
+              </TouchableOpacity>
+            </View>
           </View>
+        </ScrollView>
+      )}
+      {isSearchPerformed && (
+        <View style={{ padding: 20 }}>
+          <Text>Search results for: {searchText}</Text>
         </View>
-      </ScrollView>)}
+      )}
     </SafeAreaView>
   );
 };
